b4cffa57dac87807adf3d08dad45b4d0a2d3ebff
Horrgs <horrgs34@gmail.com>
2015-03-18 23:04:25 -0400

    WGAlert - for severe weather alerts e.g. TORNADOS

---
+++ src/org/horrgs/weathertweets/wunderground/WGAlert.java
@@ -0,0 +1,13 @@
+package org.horrgs.weathertweets.wunderground;
+
+/**
+ * Created by Horrgs on 3/14/2015.
+ */
+public interface WGAlert {
+
+    public String getDescription();
+
+    public String dateSet();
+
+    public String dateExpires();
+}
