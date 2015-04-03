398e4c39f017e30dc8cd0da2cbafedf91b1a08cf
Horrgs <horrgs34@gmail.com>
2015-04-03 17:28:15 -0400

    Another step towards multi weather tweets.

--- src/org/horrgs/weathertweets/WeatherTweets.java
+++ src/org/horrgs/weathertweets/WeatherTweets.java
@@ -9,9 +9,7 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.ProtocolException;
 import java.util.Calendar;
 import java.util.concurrent.Executors;
@@ -23,9 +21,9 @@ import java.util.concurrent.TimeUnit;
  */
 public class WeatherTweets implements Runnable {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File f = new File("keys.txt");
-        if(!f.exists()) {
+        if (!f.exists()) {
             try {
                 f.createNewFile();
             } catch (IOException ex) {
@@ -34,11 +32,12 @@ public class WeatherTweets implements Runnable {
         }
         System.out.println("Creating thread constructor.");
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
-        scheduledExecutorService.scheduleAtFixedRate(new WeatherTweets(), 0, 1, TimeUnit.HOURS);
+        scheduledExecutorService.scheduleAtFixedRate(new WeatherTweets(), 0, 1, TimeUnit.MINUTES);
     }
 
     @Override
     public void run() {
+        System.out.println("Running");
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(calendar.getTime());
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
@@ -46,92 +45,106 @@ public class WeatherTweets implements Runnable {
         int dayinyear = calendar.get(Calendar.DAY_OF_YEAR);
         int min = calendar.get(Calendar.MINUTE);
         boolean message = new WGLookup(WGLookup.Protocol.ALERT, "NY", "Buffalo").getMessage().equals("Zero weather alerts.");
+        System.out.println("Is the message \"Zero weather alerts.?\" " + message);
+        System.out.println("The mesage is: " + new WGLookup(WGLookup.Protocol.ALERT, "NY", "Buffalo").getMessage());
         WGLookup.Protocol protocol = WGLookup.Protocol.CONDITION;
-        switch(min) {
-            case 0:
-                protocol = WGLookup.Protocol.CONDITION;
-                break;
-            case 20:
-                if(!message) {
-                    protocol = WGLookup.Protocol.ALERT;
-                }
-                break;
-            case 30:
-                if(message) {
-                    protocol = WGLookup.Protocol.FORECAST;
-                }
-                break;
-            case 40:
-                if(!message) {
-                    protocol = WGLookup.Protocol.FORECAST;
-                }
-
-        }
         WGLookup wgLookup = new WGLookup(protocol, "NY", "Buffalo");
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject = null;
         try {
             Object obj = jsonParser.parse(new FileReader("secrets.json"));
             jsonObject = (JSONObject) obj;
+            System.out.println("Parsing secrets.json ....");
         } catch (Exception ex) {
             ex.printStackTrace();
             return;
         }
 
+        String format = "[" + hour + "/" + dayinyear + "/" + year + "]\n";
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
         configurationBuilder.setDebugEnabled(true)
-            .setOAuthConsumerKey(jsonObject.get("OAuthConsumerKey").toString())
-            .setOAuthConsumerSecret(jsonObject.get("OAuthConsumerSecret").toString())
-            .setOAuthAccessToken(jsonObject.get("OAuthAccessToken").toString())
-            .setOAuthAccessTokenSecret(jsonObject.get("OAuthAccessTokenSecret").toString());
+                .setOAuthConsumerKey(jsonObject.get("OAuthConsumerKey").toString())
+                .setOAuthConsumerSecret(jsonObject.get("OAuthConsumerSecret").toString())
+                .setOAuthAccessToken(jsonObject.get("OAuthAccessToken").toString())
+                .setOAuthAccessTokenSecret(jsonObject.get("OAuthAccessTokenSecret").toString());
         TwitterFactory tf = new TwitterFactory(configurationBuilder.build());
         Twitter twitter = tf.getInstance();
         try {
-            twitter.updateStatus("[" + hour + "/" + dayinyear + "/" + year + "]\n" +
-            "Temp: " + wgLookup.getFTemp() + "F\n" +
-            "Feels Like: " + wgLookup.getFeelsLike() + "F\n" +
-            "Humidity: " + wgLookup.getHumidity() + "\n" +
-            "Forecast: " + wgLookup.getForecast() + "\n" +
-            "Precip: " + wgLookup.getPrecipitation()  + " inches\n"  +
-            "Wind: " + wgLookup.getWind() + "MPH\n" +
-            "Wind Gusts: "  + wgLookup.getWindGusts() + "MPH");
-
-        } catch (TwitterException ex) {
-            ex.printStackTrace();
-        }
-
-        try {
-            String format = "[" + hour + "/" + dayinyear + "/" + year + "]\n";
-            switch(protocol) {
-                case CONDITION:
+            switch(min) {
+                case 0:
+                    protocol = WGLookup.Protocol.CONDITION;
+                    wgLookup.setProtocol(protocol);
+                    System.out.println("Protocol updated to " + protocol.getProtocolType());
+                    System.out.println("A weather" + protocol.getProtocolType() + "tweet is being sent.");
                     twitter.updateStatus(format +
                             "Temp: " + wgLookup.getFTemp() + "F\n" +
                             "Feels Like: " + wgLookup.getFeelsLike() + "F\n" +
                             "Humidity: " + wgLookup.getHumidity() + "\n" +
                             "Forecast: " + wgLookup.getForecast() + "\n" +
-                            "Precip: " + wgLookup.getPrecipitation()  + " inches\n"  +
+                            "Precip: " + wgLookup.getPrecipitation() + " inches\n" +
                             "Wind: " + wgLookup.getWind() + "MPH\n" +
-                            "Wind Gusts: "  + wgLookup.getWindGusts() + "MPH");
+                            "Wind Gusts: " + wgLookup.getWindGusts() + "MPH");
                     break;
-                case ALERT:
-                    //TODO: this will need to check for multiple alerts.
-                    break;
-                case FORECAST:
-                    if(wgLookup.shouldTweetSimplistic()) {
-                        twitter.updateStatus(format +
-                                "Outlook: " + wgLookup.getPrediction() + "\n" +
-                                "Chance of Precipitation: " + wgLookup.getPrecipitationPossibility() + "%");
-                    } else {
-                        twitter.updateStatus(format +
-                                "Temp: " + wgLookup.getAccuHighFahrenheit() + "F/" + wgLookup.getAccuLowFahrenheit() + "F\n" +
-                                "Outlook: " + wgLookup.getAccuConditions() + "\n" +
-                                "Chance of Precipitation: " + wgLookup.getAccuPrecipPossibility() + "%\n" +
-                                "Wind: " + wgLookup.getMaxWind()  + "MPH max / " + wgLookup.getAvgWind() + "MPH avg\n" +
-                                "Humidity: " + wgLookup.getAvgHumidity() + "%");
-                    }
-            }
-        } catch (TwitterException ex) {
+                case 20:
+                    if (!message) {
+                        protocol = WGLookup.Protocol.ALERT;
+                        wgLookup.setProtocol(protocol);
+                        //TODO: this will need to check for multiple alerts.
+                        System.out.println("Protocol updated to " + protocol.getProtocolType());
+                        System.out.println("A weather" + protocol.getProtocolType() + "tweet is being sent.");
+                        twitter.updateStatus("WEATHER ALERT\n" +
+                                "Description: " + wgLookup.getDescription() + "\n" +
+                                "Began At: " + wgLookup.dateSet() +  "\n" +
+                                "Expires At: " + wgLookup.dateExpires());
 
+                    }
+                    break;
+                case 30:
+                    if (message) {
+                        protocol = WGLookup.Protocol.FORECAST;
+                        wgLookup.setProtocol(protocol);
+                        System.out.println("Protocol updated to " + protocol.getProtocolType());
+                        System.out.println("A weather" + protocol.getProtocolType() + "tweet is being sent.");
+                        if (wgLookup.shouldTweetSimplistic()) {
+                            twitter.updateStatus(format +
+                                    "Outlook: " + wgLookup.getPrediction() + "\n" +
+                                    "Chance of Precipitation: " + wgLookup.getPrecipitationPossibility() + "%");
+                        } else {
+                            System.out.println("A weather" + protocol.getProtocolType() + "tweet is being sent.");
+                            wgLookup.setProtocol(protocol);
+                            twitter.updateStatus(format +
+                                    "Temp: " + wgLookup.getAccuHighFahrenheit() + "F/" + wgLookup.getAccuLowFahrenheit() + "F\n" +
+                                    "Outlook: " + wgLookup.getAccuConditions() + "\n" +
+                                    "Chance of Precipitation: " + wgLookup.getAccuPrecipPossibility() + "%\n" +
+                                    "Wind: " + wgLookup.getMaxWind() + "MPH max / " + wgLookup.getAvgWind() + "MPH avg\n" +
+                                    "Humidity: " + wgLookup.getAvgHumidity() + "%");
+                        }
+                    }
+                    break;
+                case 40:
+                    if (!message) {
+                        protocol = WGLookup.Protocol.FORECAST;
+                        wgLookup.setProtocol(protocol);
+                        System.out.println("Protocol updated to " + protocol.getProtocolType());
+                        System.out.println("A weather" + protocol.getProtocolType() + "tweet is being sent.");
+                        if (wgLookup.shouldTweetSimplistic()) {
+                            twitter.updateStatus(format +
+                                    "Outlook: " + wgLookup.getPrediction() + "\n" +
+                                    "Chance of Precipitation: " + wgLookup.getPrecipitationPossibility() + "%");
+                        } else {
+                            System.out.println("Updating status..");
+                            twitter.updateStatus(format +
+                                    "Temp: " + wgLookup.getAccuHighFahrenheit() + "F/" + wgLookup.getAccuLowFahrenheit() + "F\n" +
+                                    "Outlook: " + wgLookup.getAccuConditions() + "\n" +
+                                    "Chance of Precipitation: " + wgLookup.getAccuPrecipPossibility() + "%\n" +
+                                    "Wind: " + wgLookup.getMaxWind() + "MPH max / " + wgLookup.getAvgWind() + "MPH avg\n" +
+                                    "Humidity: " + wgLookup.getAvgHumidity() + "%");
+                        }
+                    }
+                    break;
+            }
+        } catch(TwitterException ex) {
+            ex.printStackTrace();
         }
     }
 }
